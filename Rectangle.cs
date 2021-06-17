7de8b523022260a34d4cdca90607a62a84205aa7
manpreetkaur98 <mk4223165@gmail.com>
2021-06-17 18:38:50 +0300

    Rectangle class created

---
+++ Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleProgram
+{
+    public class Rectangle
+
+    {
+        int defaultLength, defaultWidth, area, perimeter;
+        public Rectangle()
+        {
+            defaultLength = 1;
+            defaultWidth = 1;
+        }
+        public int GetLength()
+        {
+            return defaultLength;
+        }
+        public int SetLength(int length)
+        {
+            defaultLength = length;
+            return defaultLength;
+        }
+        public int GetWidth()
+        {
+            return defaultWidth;
+        }
+        public int SetWidth(int width)
+        {
+            defaultWidth = width;
+            return defaultWidth;
+        }
+        public int GetPerimeter()
+        {
+            perimeter = (defaultWidth * 2) + (defaultLength * 2);
+            return perimeter;
+        }
+        public int GetArea()
+        {
+            area = defaultLength * defaultWidth;
+            return area;
+        }
+    }
+
+}
